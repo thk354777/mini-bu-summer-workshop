f5c8cb8f0a59d4527a6e468afc29c38fb4019ab3
peet <peetza_31@hotmail.com>
2025-07-26 07:20:27 +0000

    debug owner

--- main.py
+++ main.py
@@ -1,54 +1,44 @@
 from fastapi import FastAPI, Form, Request
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
-from typing import List
 from pydantic import BaseModel
+from typing import List, Optional
 
 app = FastAPI()
-
 templates = Jinja2Templates(directory="templates")
-
-# Temporary storage for todos (in-memory)
-todos: List[str] = []
 
 class Todo(BaseModel):
     id: int
     task: str
     owner: str
 
-@app.get("/", response_class=HTMLResponse)
-def read_root(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request, "todos": todos})
+todos: List[Todo] = []
 
-@app.get("/todos/{owner}", response_class=HTMLResponse)
-def get_todos_by_owner(request: Request, owner: str):
-    owner_todos = [todo for todo in todos if todo.owner == owner]
-    return templates.TemplateResponse("todos.html", {
+@app.get("/", response_class=HTMLResponse)
+def read_root(request: Request, owner: Optional[str] = None):
+    if owner:
+        filtered = [todo for todo in todos if todo.owner == owner]
+    else:
+        filtered = todos
+    return templates.TemplateResponse("index.html", {
         "request": request,
-        "owner": owner,
-        "todos": owner_todos,
+        "todos": filtered,
+        "owner": owner or ""
     })
 
-@app.post("/todos/{owner}")
-async def add_todo(owner: str, item: str = Form(...)):
-    todo_id = len(todos) + 1
-    todos.append(Todo(id=todo_id, task=item, owner=owner))
-    return RedirectResponse(url=f"/todos/{owner}", status_code=303)
-
-@app.post("/create-todo")
-def create_todo(item: str = Form(...), owner: str = Form(...)):
+@app.post("/add")
+def add_todo(item: str = Form(...), owner: str = Form(...)):
     todos.append(Todo(id=len(todos) + 1, task=item, owner=owner))
-    return RedirectResponse("/", status_code=303)
+    return RedirectResponse(url=f"/?owner={owner}", status_code=303)
 
-@app.post("/delete-todo")
-def delete_todo(item: str = Form(...)):
-    if item in todos:
-        todos.remove(item)
-    return RedirectResponse("/", status_code=303)
+@app.post("/delete")
+def delete_todo(task: str = Form(...), owner: str = Form(...)):
+    for todo in todos:
+        if todo.task == task and todo.owner == owner:
+            todos.remove(todo)
+            break
+    return RedirectResponse(url=f"/?owner={owner}", status_code=303)
 
-@app.post("/edit-todo")
-def edit_todo(old_item: str = Form(...), new_item: str = Form(...)):
-    if old_item in todos:
-        index = todos.index(old_item)
-        todos[index] = new_item
-    return RedirectResponse("/", status_code=303)
+@app.post("/change-owner")
+def change_owner(owner: str = Form(...)):
+    return RedirectResponse(url=f"/?owner={owner}", status_code=303)
